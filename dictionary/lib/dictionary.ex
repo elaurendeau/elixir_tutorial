e7e3bd7f23c9fea3aa34e394b983e792f781b2c1
Elliot Laurendeau <elliot.laurendeau@bnc.ca>
2019-05-11 02:55:56 -0400

    Exercice Write the Dictionary Module

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,5 +1,24 @@
 defmodule Dictionary do
- def hello do
-   IO.puts "Hello world!"
+  def hello do
+     IO.puts "Hello world!"
   end
+
+  def word_list do
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
+  end
+
+  def random_word do
+    Enum.random word_list()
+  end
+
+  def compare_and_reverse do
+    "had we but world enough, and time"
+    |> String.graphemes
+    |> Enum.reverse
+    |> List.to_string
+    |> String.myers_difference("had we but bacon enough, and teacle")
+  end
+
 end
