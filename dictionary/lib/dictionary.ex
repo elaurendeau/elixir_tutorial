fdc765cf5b201c0dd54d024aab53217c66bd8705
Elliot Laurendeau <elliot.laurendeau@bnc.ca>
2019-05-12 14:48:10 -0400

    excluding elixir_ls folder

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,6 +1,6 @@
 defmodule Dictionary do
   def hello do
-     IO.puts "Hello world!"
+    IO.puts("Hello world!")
   end
 
   def word_list do
@@ -8,17 +8,27 @@ defmodule Dictionary do
     |> File.read!()
     |> String.split(~r/\n/)
   end
+  def test do
+    "test"
+    |> String.upcase(:default)
+    |> IO.puts()
+  end
+
+  defp test_private do
+    IO.puts("hello world")
+  end
 
   def random_word do
-    Enum.random word_list()
+    word_list()
+    |> Enum.random()
   end
+  
 
   def compare_and_reverse do
     "had we but world enough, and time"
-    |> String.graphemes
-    |> Enum.reverse
-    |> List.to_string
+    |> String.graphemes()
+    |> Enum.reverse()
+    |> List.to_string()
     |> String.myers_difference("had we but bacon enough, and teacle")
   end
-
 end
