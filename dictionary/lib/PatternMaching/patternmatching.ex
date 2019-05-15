8c9b950de1d2ff04dd10221722ae0eb824ea6fcb
Elliot Laurendeau <elliot.laurendeau@bnc.ca>
2019-05-15 16:17:52 -0400

    Pattern matching exercices

---
+++ dictionary/lib/PatternMaching/patternmatching.ex
@@ -0,0 +1,25 @@
+defmodule Patternmatching do
+  def same({a, a}) do
+    true
+  end
+
+  def same({_, _}) do
+    false
+  end
+
+  def swap({left, right}) do
+    {right, left, 1}
+  end
+
+  def len([]), do: 0
+  def len([_head | tail]), do: 1 + len(tail)
+
+  def pair([]), do: []
+  def pair([first, second | tail]), do: [ { first + second } | pair(tail)]
+  def pair([first | tail]), do: [ { first } | pair(tail) ]
+
+
+  def even_length?([]), do: true
+  def even_length?([first, second | tail]), do: true && even_length?(tail)
+  def even_length?([first | tail]), do: false && even_length?(tail)
+end
