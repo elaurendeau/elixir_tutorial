245ac62aec36e47c96815d3ad2b2340756df1272
Elliot Laurendeau <elliot.laurendeau@bnc.ca>
2019-05-17 08:48:34 -0400

    guessing letters in the game

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -3,7 +3,8 @@ defmodule Hangman.Game do
   defstruct(
     turns_left: 7,
     game_state: :init,
-    letters: []
+    letters: [],
+    guessed_letters: MapSet.new()
   )
   def init_game() do
     %Hangman.Game{
@@ -14,6 +15,25 @@ defmodule Hangman.Game do
   def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
     { game, tally(game)}
   end
+
+  def make_move(game, guess) do
+    accept_move(game, guess, game.guessed_letters |> MapSet.member?(guess))
+  end
+
+  def accept_move(game, _guess, _letter_already_guessed = true) do
+    game
+    |> Map.put(:game_state, :letter_already_guessed)
+  end
+
+  def accept_move(game, guess, _letter_not_guessed) do
+    game
+    |> Map.put(:guessed_letters, MapSet.put(game.guessed_letters, guess))
+    |> Map.put(:turns_left, game.turns_left - 1)
+  end
+
+  # def accept_move(game, guess, _letter_not_guessed) do
+  #   game |> Map.put(:guessed_letters, MapSet.put(game, value))
+  # end
 
   def tally(_game) do
     1234
--- hangman/test/make_move_test.exs
+++ hangman/test/make_move_test.exs
@@ -8,4 +8,17 @@ defmodule MakeMoveTest do
       assert {^game, _} = Hangman.make_move(game, "x")
     end
   end
+
+  test "adding a new letter to the guessed letter" do
+    game = Hangman.new_game() |> Hangman.make_move("p")
+    assert game.guessed_letters |> MapSet.member?("p") == true
+    assert game.turns_left == 6
+  end
+
+  test "changed state after guessing the same letter twice" do
+    game = Hangman.new_game() |> Hangman.make_move("p") |> Hangman.make_move("p")
+    assert game.guessed_letters |> MapSet.member?("p") == true
+    assert game.game_state == :letter_already_guessed
+    assert game.turns_left == 6
+  end
 end
